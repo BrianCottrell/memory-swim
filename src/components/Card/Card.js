a1d5573afbf9ac16c6cd34fbf6249347b7b06087
Brian Cottrell <Brian.Cottrell.mobile@gmail.com>
2019-08-08 20:21:34 -0700

    Updated card images

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,20 +1,19 @@
 import React from 'react'
 
 const Card = ({ className, id, name, onClick }) => {
-  const image = require(`./images/${name}.svg`)
 
-  return image ? (
+  return ( 
     <li className={className}>
       <a className="Card__element" onClick={() => onClick(id, name)}>
         <div className="Card__contents Card__contents--back">
           ?
         </div>
         <div className="Card__contents Card__contents--front">
-          <img src="https://www.online-convert.com/qrcode/4e9a2faa-8fe0-44cc-9909-0c37d0f13fc5/2d50a7d5-525e-4778-ac13-69295a51194f" alt={name} />
+          <img src={"https://res.cloudinary.com/dhl3gjazr/image/upload/v1565319492/deep-dive/"+name+".png"} alt={name} />
         </div>
       </a>
     </li>
-  ) : null
+  )
 }
 
 export default Card
